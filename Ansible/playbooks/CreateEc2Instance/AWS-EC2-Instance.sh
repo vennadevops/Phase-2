236960b04fa97317ae2af0ace43dde061768a4ae
venkatasykam <venkatasykam@yahoo.com>
2018-09-25 02:51:36 +0000

    Update AWS-EC2-Instance.sh

--- Ansible/playbooks/CreateEc2Instance/AWS-EC2-Instance.sh
+++ Ansible/playbooks/CreateEc2Instance/AWS-EC2-Instance.sh
@@ -1,20 +1,13 @@
 # Reference: https://www.linuxschoolonline.com/use-ansible-to-build-and-manage-aws-ec2-instances/
 # Reference: https://www.agix.com.au/build-an-ec2-using-ansible-step-by-step/
 # Reference: https://packaging.python.org/guides/installing-using-linux-tools/
+# https://foxutech.com/how-to-create-ec2-instance-using-ansible/
+
 yum install python-pip python-wheel
 
 pip install --upgrade pip
 
-pip install boto
-
-pip install botocore
-
-pip install boto3
-
-export AWS_ACCESS_KEY_ID=AKIA4AVHEKNHJIWLZJMM
-
-export AWS_SECRET_ACCESS_KEY=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
-
+pip install boto botocore boto3
 
 
 
